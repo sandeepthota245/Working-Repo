63959c4b3d05f2b71252331641bf8b2052cdf1da
ADMIN <ADMIN@MCS-PC>
2020-08-14 07:35:20 -0700

    Committing the changes

--- pricingmodule/src/com/pricingmodule/PricingModule.java
+++ pricingmodule/src/com/pricingmodule/PricingModule.java
@@ -20,8 +20,8 @@ public class PricingModule {
 	String chain_assembly = "chain_assembly";
 	int totalprice;
 
-	// to store the customers choice y/N
-	char fram_val;
+	// to store the customers entered choice y/N
+	char frame_val;
 	char handle_bar_with_brake_val;
 	char seating_val;
 	char wheels_val;
@@ -51,41 +51,58 @@ public class PricingModule {
 		// getting the prices and products which i have declared statically above
 
 		HashMap pricelist = getPrices();
-		System.out.println("Frame : " + pricelist.get(frame));
-		fram_val = sc.next().charAt(0);
-		System.out.println("handle bar with brakes : " + pricelist.get(handle_bar_with_brakes));
+		
+		System.out.println("Frame : " + pricelist.get(frame)+ " rs");
+		
+		frame_val = sc.next().charAt(0);
+		
+		System.out.println("handle bar with brakes : " + pricelist.get(handle_bar_with_brakes)+ " rs");
+		
 		handle_bar_with_brake_val = sc.next().charAt(0);
-		System.out.println("seating : " + pricelist.get(seating));
+		
+		System.out.println("seating : " + pricelist.get(seating) + " rs");
+		
 		seating_val = sc.next().charAt(0);
-		System.out.println("wheels : " + pricelist.get(wheels));
+		
+		System.out.println("wheels : " + pricelist.get(wheels)+ " rs");
+		
 		wheels_val = sc.next().charAt(0);
-		System.out.println("chain assembly : " + pricelist.get(chain_assembly));
+		
+		System.out.println("chain assembly : " + pricelist.get(chain_assembly) + " rs");
+		
 		chain_assembly_val = sc.next().charAt(0);
 
 		// Displaying the user selected choice and detailed price of product with total
 		// cost of production of cycle
 		System.out.println("Items you have selected are ");
-		if (fram_val == 'Y' || fram_val == 'y') {
+		
+		if (frame_val == 'Y' || frame_val == 'y') {
 			System.out.println("Frame : " + pricelist.get(frame));
+		
 			totalprice = totalprice + (int) pricelist.get(frame);
 		}
 		if (handle_bar_with_brake_val == 'Y' || handle_bar_with_brake_val == 'y') {
 			System.out.println("handle bar with brakes : " + pricelist.get(handle_bar_with_brakes));
+			
 			totalprice = totalprice + (int) pricelist.get(handle_bar_with_brakes);
 		}
 		if (seating_val == 'Y' || seating_val == 'y') {
 			System.out.println("seating : " + pricelist.get(seating));
+			
 			totalprice = totalprice + (int) pricelist.get(seating);
 		}
 		if (wheels_val == 'y' || wheels_val == 'Y') {
 			System.out.println("wheels : " + pricelist.get(wheels));
+			
 			totalprice = totalprice + (int) pricelist.get(wheels);
 		}
 		if (chain_assembly_val == 'y' || chain_assembly_val == 'Y') {
 			System.out.println("chain assembly : " + pricelist.get(chain_assembly));
+		
 			totalprice = totalprice + (int) pricelist.get(chain_assembly);
 		}
-		System.out.println("total price for  production is :: " + totalprice + "rs");
+		
+		System.out.println("total price for  production is :: " + totalprice + " rs");
 	}
 
 	public static void main(String[] args) {
